c7d1c41a4b761993483584a50f0d17159d90c9e7
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-11-23 15:54:45 +0700

    membuat function first node

--- Exercise 3 Semester 3/Program.cs
+++ Exercise 3 Semester 3/Program.cs
@@ -59,7 +59,13 @@ namespace Exercise_3_Semester_3
                 Console.Write(LAST.rollNumber + "    " + LAST.name + "\n");
             }
         }
-        
+       public void firstNode()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+                Console.WriteLine("\nThe first record in the list is:\n\n " + LAST.next.rollNumber + "   " + LAST.next.name);
+        }
             
     }
         
