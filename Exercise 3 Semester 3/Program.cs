48f7246a2550daa4ffbdea902df3c367dc77ebf5
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-11-23 16:07:30 +0700

    membuat function Main

--- Exercise 3 Semester 3/Program.cs
+++ Exercise 3 Semester 3/Program.cs
@@ -80,6 +80,51 @@ namespace Exercise_3_Semester_3
                     Console.WriteLine("4. Exit");
                     Console.WriteLine("\nEnter your choice (1-4): ");
                     char ch = Convert.ToChar(Console.ReadLine());
+                    switch(ch)
+                    {
+                        case '1':
+                            {
+                                obj.traverse();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.Write("\nEnter the roll number of the student whose record is to be searched: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number: " + curr.rollNumber);
+                                    Console.WriteLine("\nName: " + curr.name);
+                                }
+                            }
+                            break;
+                        case '3':
+                            {
+                                obj.firstNode();
+                            }
+                            break;
+                        case '4':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("Invalid Option");
+                                break;
+                            }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
                 }
             }
         }
