672ef32e8da98023819943ea0f25ff5518df967f
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-11-23 15:52:36 +0700

    membuat function traverse

--- Exercise 3 Semester 3/Program.cs
+++ Exercise 3 Semester 3/Program.cs
@@ -42,7 +42,24 @@ namespace Exercise_3_Semester_3
                 return false;
         }
 
-        public void traverse()
+        public void traverse()//Traverses all the nodes of the list
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+            {
+                Console.WriteLine("\nRecords in the list are: \n");
+                Node currentNode;
+                currentNode = LAST.next;
+                while (currentNode != LAST)
+                {
+                    Console.Write(currentNode.rollNumber + "   " + currentNode.name + "\n");
+                    currentNode = currentNode.next;
+                }
+                Console.Write(LAST.rollNumber + "    " + LAST.name + "\n");
+            }
+        }
+        
             
     }
         
